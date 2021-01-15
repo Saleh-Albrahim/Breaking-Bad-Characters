8ae64785a1c67f974f45f13f00c15eff3ef3c23c
Saleh Alibrahim <s.albrahim@outlook.com>
2021-01-15 21:03:11 +0300

    feat: add the Search bar and the Spinner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/ui/Header';
 import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 import './App.css';
 
 const App = () => {
 
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const response = await fetch(`https://www.breakingbadapi.com/api/characters`)
+      const response = await fetch(`https://www.breakingbadapi.com/api/characters?name=${query}`)
       const data = await response.json();
       setItems(data);
       setIsLoading(false);
     }
     fetchItems();
-  }, [])
+  }, [query])
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => { setQuery(q) }} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import SpinnerImg from '../../img/spinner.gif';
+
+const Spinner = () => {
+    return <img style={{ width: '200px', margin: 'auto', display: 'block' }} src={SpinnerImg} alt='Loading' />
+
+}
+
+export default Spinner
