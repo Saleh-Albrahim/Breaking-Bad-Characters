7f54fabfffcc30c56466097a5458bbd0f0dde069
Saleh Alibrahim <s.albrahim@outlook.com>
2021-01-15 20:33:11 +0300

    feat: add CharacterGrid & CharaterItems component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 import './App.css';
 
 const App = () => {
+
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch(`https://www.breakingbadapi.com/api/characters`)
+      const data = await response.json();
+      setItems(data);
+      setIsLoading(false);
+    }
+    fetchItems();
+  }, [])
+
   return (
     <div className="container">
-      <Header></Header>
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharacterItem = ({ item }) => {
+    return (
+        <>
+            <h1>{item.name}</h1>
+        </>
+    )
+}
+
+export default CharacterItem
